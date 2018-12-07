55a05063ce4202dbcdc0880930666fe66fa1aad6
Tiago Borges <tiago@petcoach.co>
2018-12-07 10:22:22 -0300

    wip

--- setup.py
+++ setup.py
@@ -13,10 +13,9 @@ else:  # assume windows
 with io.open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
-metadata, options = get_config()
-metadata['ext_modules'] = [
-    setuptools.Extension("MySQLdb._mysql", sources=['MySQLdb/_mysql.c'], **options)
-]
+metadata = {}
+options = {}
+metadata['ext_modules'] = []
 metadata['long_description'] = readme
 metadata['long_description_content_type'] = "text/markdown"
 setuptools.setup(**metadata)
